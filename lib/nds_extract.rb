a17305be218be10307bd25d4504369db1304e41e
Tisdale Fry <tisdalefry@gmail.com>
2020-02-27 00:50:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,7 +41,7 @@ def total_gross(source)
   idx = 0
   total = 0
   while idx < source.length do
-    total += list_of_directors(source)[directors_totals(source)]
+    total += list_of_directors(idx)[directors_totals(source)]
     idx += 1
   end  
   # Write this implementation
